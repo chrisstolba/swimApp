1e1aed918e58f026b35acd71b9ff88f7c6c0a0a3
Chris Stolba <stolbac@gmail.com>
2020-01-15 18:22:37 -0800

    Initial setup for endpoints done. Logic requires mens and womens tables be seperate for races and lineups so I adjusted the SQL database creation scripts to reflect.

---
+++ database/createTable-races_men.sql
@@ -0,0 +1,13 @@
+create table races_men (
+  id bigint unsigned not null unique auto_increment,
+  meetid bigint unsigned not null,
+  eventid bigint unsigned not null,
+  swimmerid bigint unsigned not null,
+  time decimal(12,2) not null,
+
+  primary key (id),
+
+  foreign key (meetid) references meets(id),
+  foreign key (eventid) references events(id),
+  foreign key (swimmerid) references swimmers(id)
+);
